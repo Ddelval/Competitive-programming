d6b2a9b2664dd953d164afe9b909a0ee1def07ff
Ddelval <delvaldavid1@gmail.com>
2019-08-05 22:02:34 +0200

    Segment tree2

--- Uva/11402/11402/main.cpp
+++ Uva/11402/11402/main.cpp
@@ -59,11 +59,113 @@ typedef vector<int> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+vi tree;
+vector<char> lazy;
+string s;
+void build (int k,int l,int r){
+	if(l==r){
+		tree[k]=s[l]-'0';
+		return;
+	}
+	int mid=(r+l)>>1;
+	build(k<<1,l,mid);
+	build(k<<1|1,mid+1,r);
+	tree[k]=tree[k<<1]+tree[k<<1|1];
+}
+void propagate (int n,char code){
+	if(n>=4*s.length())return;
+	if(lazy[n]){
+		if(code!='I'){
+			lazy[n]=code;
+		}
+		else{
+			if(lazy[n]=='E')lazy[n]='F';
+			else if(lazy[n]=='F') lazy[n]='E';
+			else lazy[n]=0;
+		}
+	}
+	else lazy[n]=code;
+	
+}
+void laz(int k,int l,int r){
+	if(k>=4*s.length())return;
+	if(lazy[k]){
+		char code=lazy[k];
+		if(code=='F'){
+			tree[k]=r-l+1;
+			
+		}
+		else if(code=='E'){
+			tree[k]=0;
+			
+		}
+		else{
+			tree[k]=r-l+1-tree[k];
+			
+		}
+		lazy[k]=0;
+		propagate(k<<1, code);
+		propagate(k<<1|1, code);
+		
+	}
+}
 
+int querry(int k,int l,int r, int i,int j){
+	laz(k,l,r);
+	if(i>r||j<l) return 0;
+	if(l>=i&&r<=j)return tree[k];
+	int mid=(r+l)>>1;
+	return querry(k<<1|1,mid+1,r,i,j)+querry(k<<1, l, mid, i, j);
+}
 
+void update(int k,int l,int r,int i,int j,char code){
+	laz(k,l,r);
+	if(i>r||j<l)return;
+	if(l>=i&&r<=j){
+		lazy[k]=code;
+		laz(k, l, r);
+	}
+	else{
+		int mid=(r+l)>>1;
+		update(k<<1,l,mid,i,j,code);
+		update(k<<1|1,mid+1,r,i,j,code);
+		tree[k]=tree[k<<1]+tree[k<<1|1];
+	}
+}
 int main(){
     ios::sync_with_stdio(false);
-	
+	int q;
+	cin>>q;
+	REP(z,q){
+		s="";
+		int m; cin>>m;
+		int t;
+		string st;
+		REP(i,m){
+			cin>>t;
+			cin>>st;
+			REP(j,t){
+				s.append(st);
+			}
+		}
+		int q2; cin>>q2;
+		tree=vi(s.length()*4);
+		lazy=vector<char>(s.length()*4);
+		build(1, 0, (int)s.length()-1);
+		char c; int a,b;
+		cout<<"Case "<<z+1<<":\n";
+		int count=1;
+		REP(i,q2){
+			cin>>c>>a>>b;
+			if(c!='S'){
+				update(1, 0, (int)s.length()-1, a, b,c);
+			}
+			else{
+				cout<<"Q"<<count<<": "<<querry(1, 0, (int)s.length()-1, a, b)<<"\n";
+				count++;
+			}
+		}
+	}
 
     return 0;
 }
