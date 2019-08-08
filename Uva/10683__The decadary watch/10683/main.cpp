b7d9f0f994d7a9874364522238cd0fa5924f9bdd
David <delvaldavid1@gmail.com>
2019-08-08 23:58:40 +0200

    20190808_2

---
+++ Uva/10683__The decadary watch/10683/main.cpp
@@ -0,0 +1,88 @@
+// UVa Online Judge 10683: The decadary watch
+//  10683
+//	main.cpp
+//  Created by David del Val on 08/08/2019
+//
+//
+
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <math.h>
+#include <utility>
+#include <string.h>
+#include <limits.h>
+
+using namespace std;
+
+#define mp(x, y) make_pair(x, y)
+#define pb(x) push_back(x)
+#define fi first
+#define se second
+#define echobin(x) cout<<#x<<":"<<x<<" ="<<bitset<8>(x)<<"  ";
+#define echo(...) {cout<<"->";ECHO(#__VA_ARGS__, __VA_ARGS__ );}
+#define REPO(i,a,b) for(int i=a;i<b;i++)
+#define REP(i,b) for(int i=0;i<b;i++)
+
+void ECHO(string _s){cout<<endl;(void)_s;}
+template<typename T, typename ...Args> void ECHO(string _s, T x, Args... args){
+    unsigned _i; string _s2="";
+    for(_i=0;_i<_s.length();++_i){if(_s[_i]==',')break;if(_s[_i]!=' ')_s2+=_s[_i];}
+    if(_i==_s.length()){--_i;}cout<<"  ("<<_s2<<"):"<<x;
+    ECHO(_s.substr(_i+1, _s.length()-_i-1), args...);
+}
+
+
+template<typename T0, typename T1>
+inline ostream& operator << (ostream& os, pair<T0, T1>& p){
+    return os << "(" << p.first << ", " << p.second <<")";
+}
+
+template <typename T>
+inline ostream& operator << (ostream& os, vector<T>& v){
+    for(unsigned i = 0; i < v.size(); ++i){cout << v[i] << "_";}cout<<endl;
+    return os;
+}
+
+template<typename T> inline T _min(T x1, T x2, T x3){return min(x1, min(x2, x3));}
+template<typename T> inline T _min(T x1, T x2, T x3, T x4){return min(min(x1, x2), min(x2, x3));}
+
+//gcd(0, n) = n
+inline int _gcd(int a, int b){ while(b) b %= a ^= b ^= a ^= b; return a;}
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+	string lin;
+	int h,m,s,c,dc,ds,dm,dh,time,dtime;
+	while(getline(cin,lin)){
+		h=stoi(lin.substr(0,2));
+		m=stoi(lin.substr(2,2));
+		s=stoi(lin.substr(4,2));
+		c=stoi(lin.substr(6,2));
+		time=c+100*(s+60*(m+60*(h)));
+		dtime=(int)(time*(10*100*100*100.0)/(24*3600*100.0));
+		dc=dtime%100;
+		dtime/=100;
+		ds=dtime%100;
+		dtime/=100;
+		dm=dtime%100;
+		dtime/=100;
+		dh=dtime;
+		printf("%d%.2d%.2d%.2d\n",dh,dm,ds,dc);
+	}
+
+    return 0;
+}
+
