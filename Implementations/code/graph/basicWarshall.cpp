e4d65d5bca039d5d43abfbef2903ad7cb93a42ac
David <delvaldavid1@gmail.com>
2021-07-03 23:21:21 +0200

    Warshall + rounds 729,728

---
+++ Implementations/code/graph/basicWarshall.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+
+const int lim = 1;
+int adyMat[lim][lim];
+void warshall(int n) {
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                adyMat[i][j] = min(adyMat[i][j], adyMat[i][k] + adyMat[k][j]);
+            }
+        }
+    }
+}
