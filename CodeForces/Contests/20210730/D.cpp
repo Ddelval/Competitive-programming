eac8bf810e73ddac8a588f5b80fcdb456f162534
David <delvaldavid1@gmail.com>
2021-07-30 22:40:01 +0200

    edu 112

---
+++ CodeForces/Contests/20210730/D.cpp
@@ -0,0 +1,157 @@
+//  D.cpp
+//  Created by David del Val on 30/07/2021
+//
+//
+// https://github.com/Ddelval/Competitive-programming/blob/master/template.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+
+template <typename T, typename Q>
+inline ostream &operator<<(ostream &o, pair<T, Q> p);
+
+// ====================================================== //
+// ===================  Container IO  =================== //
+// ====================================================== //
+template <bool B, typename T = void>
+using Enable_if = typename std::enable_if<B, T>::type;
+struct subs_fail {};
+
+template <typename T> struct subs_succeeded : std::true_type {};
+template <> struct subs_succeeded<subs_fail> : std::false_type {};
+
+template <typename T> struct get_iter_res {
+private:
+    template <typename X> static auto check(X const &x) -> decltype(x.begin());
+    static subs_fail check(...);
+
+public:
+    using type = decltype(check(std::declval<T>()));
+};
+
+template <typename T>
+struct Has_iterator : subs_succeeded<typename get_iter_res<T>::type> {};
+template <> struct Has_iterator<string> : subs_fail {};
+
+constexpr const char *sep1 = " ";
+constexpr const char *sep2 = "\n";
+template <typename T> struct get_termination {
+    static constexpr const char *get() { return sep1; }
+};
+template <typename U, typename S> struct get_termination<vector<U, S>> {
+    static constexpr const char *get() { return sep2; }
+};
+
+template <typename T>
+Enable_if<Has_iterator<T>::value, ostream &> operator<<(ostream &o, T val) {
+    bool first = true;
+    for (auto it = val.begin(); it != val.end(); ++it) {
+        if (!first) {
+            constexpr const char *terminator =
+                get_termination<typename T::value_type>::get();
+            o << terminator;
+        }
+        first = false;
+        o << *it;
+    }
+    return o;
+}
+
+template <typename T = ll> inline vector<T> readVector(int size) {
+    vector<T> v;
+    v.reserve(size);
+    T a;
+    for (int i = 0; i < size; ++i) {
+        cin >> a;
+        v.push_back(a);
+    }
+    return v;
+}
+
+// ====================================================== //
+// ================== Pairs operations ================== //
+// ====================================================== //
+inline pii operator+(pii a, pii b) { return {a.fi + b.fi, a.se + b.se}; }
+
+template <typename T, typename Q>
+inline ostream &operator<<(ostream &o, pair<T, Q> p) {
+    o << "(" << p.fi << "," << p.se << ")";
+    return o;
+}
+
+// gcd(0, n) = n
+inline long long _gcd(long long a, long long b) {
+    while (b)
+        b %= a ^= b ^= a ^= b;
+    return a;
+}
+
+ll inf = LLONG_MAX / 10;
+int iinf = INT_MAX / 10;
+
+#ifdef _LOCAL_
+// Local constraints
+
+#else
+// Judge constraints
+#endif
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    vector<vi> differences(6, vi(n, 0));
+    string pat1 = "abc";
+    string pat2 = "acb";
+    for (int offset = 0; offset < 3; ++offset) {
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != pat1[(i + offset) % 3]) {
+                differences[offset][i] = 1;
+            }
+            if (s[i] != pat2[(i + offset) % 3]) {
+                differences[offset + 3][i] = 1;
+            }
+        }
+    }
+    vector<vi> presums(6, vi());
+    for (int i = 0; i < 6; ++i) {
+        presums[i].reserve(n + 1);
+        presums[i].pb(0);
+
+        for (int j = 0; j < n; ++j) {
+            presums[i].pb(presums[i].back() + differences[i][j]);
+        }
+    }
+
+    int a, b;
+    while (m--) {
+        cin >> a >> b;
+        a--, b--;
+        int mi = iinf;
+        for (int i = 0; i < 6; ++i) {
+            mi = min(mi, presums[i][b + 1] - presums[i][a]);
+        }
+        cout << mi << "\n";
+    }
+
+    return 0;
+}
